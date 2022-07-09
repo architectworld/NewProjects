8837babf9e9c96add6a6f7a1d7e84f8f8a496b83
Shibbu <kellerjackrich@gmail.com>
2022-07-09 12:38:20 +0530

    initial commit

---
+++ src/main/java/com/first/demoSpringProj/DemoSpringProjApplication.java
@@ -0,0 +1,13 @@
+package com.first.demoSpringProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/demoSpringProj/DemoSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.demoSpringProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
